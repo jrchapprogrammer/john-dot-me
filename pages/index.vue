0179176d538c8aac90303b9670c38d3b1e821812
John Chappelle <jrchap82@gmail.com>
2018-12-13 15:42:40 -0500

    clean: clean up index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="section is-large<!-- philly hero is-fullheight-with-navbar -->">
+  <section class="section is-large hero is-fullheight">
     <div class="container has-text-centered">
       <h1 class="title">Hi, I'm John Chappelle</h1>
       <h2 class="subtitle">a Freelance Web Developer based in Philadelphia, PA.</h2>
@@ -8,48 +8,10 @@
 </template>
 
 <script>
-// import Logo from '~/components/Logo.vue'
-
 export default {
   layout: 'home'
-  // components: {
-  //   Logo
-  // }
 }
 </script>
 
 <style>
-.philly {
-  background-image: url('https://res.cloudinary.com/dn0k1nlvu/image/upload/c_scale,w_1703/v1544480200/master-wen-670290-unsplash_vtda1u.jpg');
-  background-size: cover;
-}
-/* .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-} */
 </style>
